bbe41bb4bd3bb7218b8fed1d1c001b97a9d3c6e5
Tien Le and Travis Wainwright <travis@>
2019-01-15 11:03:36 -0800

    add toggle, and cursor, add facts to page

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,14 @@ $(document).ready(function() {
   $("p").click(function() {
     $(".frog-showing").fadeToggle();
     $(".frog-hidden").fadeToggle();
+
+  });
+});
+
+$(document).ready(function() {
+  $("h1").click(function() {
+    $(".facts-hidden").show();
+    $(".facts-showing").fadeToggle();
+
   });
 });
